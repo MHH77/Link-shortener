122a8db69ff4e48db7ba199ba55babc4aa1859f9
MHH77 <mesfandiari77@gmail.com>
2025-03-09 04:41:21 +0330

    [feature] add swagger explanation

--- src/main/java/org/mhh/config/WebConfig.java
+++ src/main/java/org/mhh/config/WebConfig.java
@@ -2,6 +2,7 @@ package org.mhh.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -11,5 +12,11 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/api/**")
                 .allowedOrigins("http://localhost:3000") // آدرس React
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // Add this handler *first* to ensure it takes precedence
+        registry.addResourceHandler("/**");
     }
 }
